eb03321c53a8aa37341c87565a16cec59ae8a47c
april-guerin <aprilguerin@yahoo.com>
2018-07-24 19:28:45 +0000

    automatically backed up by learn

--- holiday_greeting.rb
+++ holiday_greeting.rb
@@ -2,7 +2,7 @@ def holiday_greeting(to = "Mom", from = "Your Favorite Child", holiday = "Mother
   "Happy #{holiday}, #{to}! - From #{from}"
 end
 
-holiday_greeting()
-holiday_greeting("Beyonce", "Jay-Z", "Fourth of July")
+puts holiday_greeting()
+puts holiday_greeting("Beyonce", "Jay-Z", "Fourth of July")
 
   
